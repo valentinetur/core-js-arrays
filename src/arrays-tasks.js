e521d931dad9a411febd70cf41b017cfb328a0d8
Valentine Tur <valentine.tur.it@gmail.com>
2025-01-13 23:19:47 +0100

    feat: add getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -422,8 +422,10 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, rowIndex) =>
+    Array.from({ length: n }, (el, colIndex) => (rowIndex === colIndex ? 1 : 0))
+  );
 }
 
 /**

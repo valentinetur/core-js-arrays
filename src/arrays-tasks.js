65250e91cc0e5fce305c9cb279b391da8f6bb958
Valentine Tur <valentine.tur.it@gmail.com>
2025-01-14 00:24:08 +0100

    feat: add findLongestIncreasingSubsequence task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -507,8 +507,23 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  if (nums.length === 0) return 0;
+
+  let maxLen = 1;
+  let currLen = 1;
+
+  nums.reduce((prev, curr) => {
+    if (curr > prev) {
+      currLen += 1;
+      maxLen = Math.max(maxLen, currLen);
+    } else {
+      currLen = 1;
+    }
+    return curr;
+  });
+
+  return maxLen;
 }
 
 /**

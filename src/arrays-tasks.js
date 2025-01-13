0436409c12cd6b051d41f9652d771c13252d666f
Valentine Tur <valentine.tur.it@gmail.com>
2025-01-13 23:32:57 +0100

    feat: get Odd Numbers task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -439,8 +439,13 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((el, index) => {
+      if (el % 2 !== 0) return index;
+      return -1;
+    })
+    .filter((el) => el !== -1);
 }
 
 /**
